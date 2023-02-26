2571dd44b50dfc13305f54aa2c74eb6adaf011a4
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-02-26 15:09:45 -0300

    atualização

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 
-//Atribuindo evento de click no botão Mostrar Mais/Menos através de um laço de repetição com For.
-
 const botao = document.querySelectorAll(".btn");
 const texto = document.querySelectorAll(".card-texto-2");
 
-for(let i = 0; i < 6; i++){
-    botao[i].addEventListener("click", function() {
-        if(texto[i].style.display === 'none' && botao[i].innerHTML === "Mostrar Mais"){
-            texto[i].style.display = 'flex'; botao[i].innerHTML = "Mostrar Menos"
-        }else{
-            texto[i].style.display = 'none'; botao[i].innerHTML = "Mostrar Mais"
-        }
-    })
-}
+// Utilizando o método forEach.
+botao.forEach((btn, i) => {
+    btn.addEventListener("click", function() {
+      if (texto[i].style.display === "none" || btn.innerHTML === "Mostrar Mais") {
+        texto[i].style.display = "flex";
+        btn.innerHTML = "Mostrar Menos";
+      } else {
+        texto[i].style.display = "none";
+        btn.innerHTML = "Mostrar Mais";
+      }
+    });
+  });
+
+  // Com um Loop de repetição For.
+/*
+    for(let i = 0; i < botao.length; i++){
+        botao[i].addEventListener("click", function() {
+            if(texto[i].style.display === 'none' || botao[i].innerHTML === "Mostrar Mais"){
+                texto[i].style.display = 'flex'; botao[i].innerHTML = "Mostrar Menos"
+            }else{
+                texto[i].style.display = 'none'; botao[i].innerHTML = "Mostrar Mais"
+            }
+        })
+    } 
+*/
 
